3e1259a7c40ccd63a4d47d91cb783ee231980753
Abhishek Deshmukh <deshmukhabhi02@gmail.com>
2024-06-21 14:23:01 +0200

    Updated files

--- catkin_ws/src/detection/src/detect_barell.py
+++ catkin_ws/src/detection/src/detect_barell.py
@@ -16,23 +16,55 @@ class Detect:
     
     
     def image_callback(self, msg):
-        cv_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding="bgr8")  
+        cv_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding="bgr8")
         
         hsv_image = cv2.cvtColor(cv_image, cv2.COLOR_BGR2HSV)
         
-        lower_range = np.array([5, 100, 100])
-        upper_range = np.array([15, 255, 255])
+        lower_orange = np.array([5, 100, 100])
+        upper_orange = np.array([15, 255, 255])
         
-        mask = cv2.inRange(hsv_image, lowerb=lower_range, upperb=upper_range)
-
-        contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        sensitivity = 15
+        lower_white = np.array([0,0,255-sensitivity])
+        upper_white = np.array([255,sensitivity,255])
+        lower_white = np.array([0,0,168])
+        upper_white = np.array([172,111,255])
         
-        if contours:    
-            max_contour = max(contours, key=cv2.contourArea)
-            x, y, w ,h = cv2.boundingRect(max_contour)
-            cv2.rectangle(cv_image, (x, y), (x+w, y+h), (0, 255, 0), 3)
-        self.res = cv_image
-
+        orange_mask = cv2.inRange(hsv_image, lowerb=lower_orange, upperb=upper_orange)
+        white_mask = cv2.inRange(hsv_image, lowerb=lower_white, upperb=upper_white)
+        
+        combined_mask = cv2.bitwise_or(orange_mask, white_mask)
+        
+        kernel = np.ones((5, 5), np.uint8)
+        combined_mask = cv2.morphologyEx(combined_mask, cv2.MORPH_CLOSE, kernel)
+        combined_mask = cv2.morphologyEx(combined_mask, cv2.MORPH_OPEN, kernel)
+        
+        contour, _  = cv2.findContours(combined_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        
+        if contour:
+            max_contour = max(contour, key=cv2.contourArea)
+            x, y, w, h = cv2.boundingRect(max_contour)
+            cv2.rectangle(cv_image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+            
+        # lower_orange = np.array([5, 150, 150])
+        # upper_orange = np.array([15, 255, 255])
+        # sensitivity = 15
+        # lower_white = np.array([0,0,255-sensitivity])
+        # upper_white = np.array([255,sensitivity,255])
+        # lower_white = np.array([0,0,168])
+        # upper_white = np.array([172,111,255])
+        
+        # orange_mask = cv2.inRange(hsv_image, lowerb=lower_orange, upperb=upper_orange)  
+        # white_mask = cv2.inRange(hsv_image, lowerb=lower_white, upperb=upper_white)
+                      
+        # contour, _  = cv2.findContours(white_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        
+        # if contour:
+        #     max_contour = max(contour, key=cv2.contourArea)
+        #     x, y, w, h = cv2.boundingRect(max_contour)
+        #     cv2.rectangle(cv_image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        
+        self.res = cv_image        
+        
     def run(self):
         cv2.namedWindow("Detection", cv2.WINDOW_NORMAL)
         rate = rospy.Rate(10)
